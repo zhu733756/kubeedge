6a8514a3f913c82afa29b8a0fa32ac5e5ada122c
root <danma@yunify.com>
2021-02-03 07:40:58 +0000

    Csidriver fixs

--- edge/pkg/edged/edged.go
+++ edge/pkg/edged/edged.go
@@ -36,6 +36,7 @@ import (
 
 	"github.com/container-storage-interface/spec/lib/go/csi"
 	"github.com/golang/protobuf/jsonpb"
+	"github.com/golang/protobuf/proto"
 	cadvisorapi "github.com/google/cadvisor/info/v1"
 	cadvisorapi2 "github.com/google/cadvisor/info/v2"
 	v1 "k8s.io/api/core/v1"
@@ -1046,6 +1047,8 @@ func (e *edged) syncPod() {
 		switch result.Content.(type) {
 		case []byte:
 			content = result.GetContent().([]byte)
+		case string:
+			content = []byte(result.GetContent().(string))
 		default:
 			content, err = json.Marshal(result.Content)
 			if err != nil {
@@ -1103,11 +1106,17 @@ func (e *edged) syncPod() {
 			if err != nil {
 				klog.Errorf("handle volume failed: %v", err)
 			} else {
-				resp := result.NewRespByMessage(&result, res)
+				m := jsonpb.Marshaler{}
+				js, err := m.MarshalToString(res.(proto.Message))
+				if err != nil {
+					klog.Errorf("failed to marshal to string with error: %s", err)
+					continue
+				}
+				resp := result.NewRespByMessage(&result, js)
 				beehiveContext.SendResp(*resp)
 			}
 		default:
-			klog.Errorf("resType is not pod or configmap or secret or volume: esType is %s", resType)
+			klog.Errorf("resType is not pod or configmap or secret or volume: resType is %s", resType)
 			continue
 		}
 	}
