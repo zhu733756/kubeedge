d8de1ca5450b6e0f04f4950ed067dad1fc0807d3
gy95 <guoyao17@huawei.com>
2021-08-12 10:02:38 +0800

    modify according to the review comments
    Signed-off-by: gy95 <guoyao17@huawei.com>

--- common/constants/default.go
+++ common/constants/default.go
@@ -140,7 +140,4 @@ const (
 	// ServerPort is the default port for the edgecore server on each host machine.
 	// May be overridden by a flag at startup in the future.
 	ServerPort = 10350
-
-	ResourceTypeServiceAccount     = "serviceaccount"
-	OperationTypeGetServiceAccount = "getserviceaccount"
 )
